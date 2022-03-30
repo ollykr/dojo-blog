a0a412d3bf8e255fd779f74d62e289c0b12d2970
ollykr <okremianski@gmail.com>
2022-03-30 19:38:25 -0400

    Composition API basics - lesson 71

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <!-- Click the button to fire up handleClick function -->
+    <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// Component object
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  // Composition API
+  // Order of running : setup, created, mounted
+  setup() {
+  console.log('setup')
+  // We can run inside any kind of JS
+  // we can grab data from somewhere or create a computed property
+
+  // vars
+  // not reactive values
+  let name = 'mario'
+  let age = 30
+
+// we can also create functions
+const handleClick = () => {
+console.log('you clicked me')
+} 
+  // to use the above vars in a template at the top of our component
+  //  return { name: name, age: age }
+  // OR
+  // return values including a function as the third parameter
+  return { name, age, handleClick }
+   } 
+  //  data() is different from the above as age: 40 is reactive, vars are not
+  //  data() {
+  //    return {
+  // reactive value reflected in a component's template right away
+  //      age: 40
+  //    }
+  //  }
+  // created hook
+  // created() {
+  // console.log('created')
+  // },
+  // mounted hook
+  // mounted() {
+  // console.log('mounted')
+  // }
 }
 </script>
