5f9639c6cab6213faf20037c83363a6516d6a2cd
ollykr <okremianski@gmail.com>
2022-04-08 16:21:02 -0400

    Make basic values reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,8 +8,7 @@
 </template>
 
 <script>
-// in composition API we import refs from Vue, it is different in options API
-// Composition API is rarelu used for manipulating DOM content, it can be done differently. But composition API refs are useful to make values reactive
+// Composition API refs are useful to make values reactive
 import { ref } from 'vue'
 // Component object
 export default {
@@ -17,35 +16,33 @@ export default {
   // Composition API
   // Order of running : setup, created, mounted
   setup() {
-  console.log(this)
-//  p as paragraph
-// below is referenced to paragraph tag <p ref="p" >My name is {{ name }} and my age is {{ age }}</p>
 // Value property is null
 // We have not returned it
-  const p = ref(null) 
-  // We can run inside any kind of JS
-  // we can grab data from somewhere or create a computed property
+  // const p = ref(null) 
 
-  // vars
-  // not reactive values
-  let name = 'mario'
-  let age = 30
+// Be default the below vars are not reactive so if we change 'mario' to 'luigi' down below, nothing changes, it won't change paragraph content in a template
+// But if we surround them with ref(), there are reactive
+// const means ref is constant but values inside refs can change, these are reactive values
+const name = ref('mario')
+const age = ref(30)
 
 // we can also create functions
 const handleClick = () => {
-  // log out p i.e paragraph tag
-  // in console we see a ref object
-  // _value: p - it is an actual DOM reference
-  // also output a p value, a template element <p>My name is mario and my age is 30</p>
-console.log(p, p.value);
-// Add CSS class "test" to that paragraph
-p.value.classList.add('test');
-// Change text content on "click me"
-p.value.textContent = 'hello, ninjas';
+  // Update values of the refs reactively
+  name.value = 'luigi'
+  age.value = 33
 } 
 //  p for refs
-  return { name, age, handleClick, p }
-   } 
+  return { name, age, handleClick }
+   }, 
+   
+  //Options API the date is reactive by default
+  
+  data() {
+    return {
+      score: 5
+    }
+  }
   
 }
 </script>
