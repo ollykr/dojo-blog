de233c2cb2b321f87e50dea2264d7d3c9ca6c1fc
ollykr <okremianski@gmail.com>
2022-04-10 20:14:30 -0400

    Use computed for filtering out search terms

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,41 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update ninja one</button>
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }} - {{ nameTwo }}</p>
-    <button @click="updateNinjaTwo">Update ninja two</button>
+    <!-- type in the field to update the value of refs -->
+    <input type="text" v-model="search">
+    <p>search term - {{ search }}</p>
+    <!-- Output name -->
+    <!-- <p>{{ name }}</p> -->
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
    </div>
 </template>
 
 <script>
 // Composition API refs are useful to make values reactive
-import { ref, reactive } from 'vue'
+// the pattern of importing only the features we need
+import { computed, ref } from 'vue'
 // Component object
 export default {
   name: 'Home',
   setup() {
-    // we can use an object as a value of refs
-      const ninjaOne = ref({ name: 'mario', age: 30})
+    // const name = computed(() => {
+    //   return 'shaun'
+    // })
+//  return { name }
+// Using with refs
+const search = ref('')
 
-// Use reactive , not ref - note: refs are better
-const ninjaTwo = reactive({ name: 'luigi', age: 35})
-// We can't use primitive values inside reactive
-// refs - we can
-const nameOne = ref('mario');
-// reactive - we can't as 'luigi' is a primitive value
-// we can't update it
-const nameTwo = reactive('luigi')
+const names = ref(['mario','yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
 
-const updateNinjaOne = () => {
-        ninjaOne.value.age = 40;
-      }
-      // To update reactive
-      const updateNinjaTwo = () => {
-    
-    // No need to use .value with reactive
-    // We access the property directly on it
-    ninjaTwo.age = 45
-    
-    }
-      return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo, nameTwo}
+// Return a computed value based on the updated search
+const matchingNames = computed(() => {
+  // return computed value (of the ref())
+  // Use .includes on a string
+  // if it returns false , the search term is removed for the array, it filteres it out
+  return names.value.filter((name) => name.includes(search.value))
+}) 
+
+return { names, search, matchingNames }
    } 
 }
 </script>
